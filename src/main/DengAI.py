1d3b75858bc8a3301b09d7cbf5edebb79fb6e342
Vasu <vasu_suren@yahoo.co.in>
2017-07-08 14:09:46 +0530

    Progress commit. Added index

--- src/main/DengAI.py
+++ src/main/DengAI.py
@@ -6,3 +6,5 @@ def data_exploration(df_data):
 if __name__ == '__main__':
     df_data = pd.read_csv(r'../../../Data/Input/dengue_features_train.csv')
     df_data_labels = pd.read_csv(r'../../../Data/Input/dengue_labels_train.csv')
+    df_data.set_index(df_data['week_start_date'] + '_' + df_data['city'], inplace=True)
+    df_data_labels.set_index(df_data['week_start_date'] + '_' + df_data['city'], inplace=True)
